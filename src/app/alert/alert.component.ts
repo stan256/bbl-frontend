4a6db9671d1da55f095564a84cc888fecf07df99
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2019-10-15 23:45:24 +0200

    Alert service

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
+import {AlertService} from './alert.service';
 
 @Component({
   selector: 'app-alert',
