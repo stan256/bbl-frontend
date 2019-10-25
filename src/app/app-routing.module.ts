b06c1dd6fc94d95c1f4e941529891ca519d2f9c3
Stanislav Studzinskyi <Stanislav.Studzinskyi@capveriant.com>
2019-10-25 16:03:29 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
+import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './guard/auth.guard';
 
 
 const routes: Routes = [
+  {path: '', component: HomeComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent},
   {path: '**', redirectTo: 'login'},
